f39d65eb322d389731bb0dbd0b0957741d537e06
WillyCasana <willycasana@hotmail.com>
2019-05-06 12:33:58 -0500

    Update mycode.js

--- scripts/mycode.js
+++ scripts/mycode.js
@@ -10,6 +10,6 @@ function onPrint(){
     var gadget = new cloudprint.Gadget();
     gadget.setPrintDocument("url","test page");
     gadget.setPrintDocument("application/pdf","PDF Doc");
-    gadget.setPrintDocument("url","Test Page","https://willycasana.github.io/impimirnube/docs/imprimir.pdf");
+    gadget.setPrintDocument("url","Test Page","http://www.juntadeandalucia.es/averroes/centros-tic/29009041/helvia/sitio/upload/trabajo_de_alejandro_magno_ivan.pdf");
     gadget.openPrintDialog();
 }
